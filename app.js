864cd6791e42db57170eedaf3a50c4ebead202e1
Linasenbye <elyneseh@gmail.com>
2024-03-25 02:25:02 +0900

    added favorite function

--- app.js
+++ app.js
@@ -7,30 +7,32 @@ const MONTH = currentMonth(new Date());
 const YEAR = new Date().getFullYear();
 
 const API_KEY = "cf6d0653-e6b1-4e5b-bfd0-467ac374bf2b";
-//премьеры месяца
+//premieres of the month
 const API_URL_PREMIERES = `https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=${YEAR}&month=${MONTH}`;
-//цифровые релизы
+//releases
 const API_URL_RELEASES = `https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=${YEAR}&month=${MONTH}&page=1`;
-//top фильмов
+//top movies
 const API_URL_TOP = "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_MOVIES&page=1";
-// ожидамые релизы
+//waiting movies
 const API_URL_CLOSE_RELEASES = "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=CLOSES_RELEASES&page=1";
-//поиск по ключевому слову
+//search 
 const API_URL_KEYWORD = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${KEYWORD}&page=1";
 
 document.addEventListener("DOMContentLoaded", () => {
     async function getMovies(url) {
         const resp = await fetch(url, {
+            method: 'GET',
             headers: {
                 "Content-Type": "application/json",
                 "X-API-KEY": API_KEY,
             },
         });
     const respData = await resp.json();
-    console.log(respData); // Add this line to inspect the response data
+    console.log(respData); // inspect the response data
+    typicalData(respData)
   }
 
-    //при выборе категории
+    //choose the categories
     const URL = [API_URL_PREMIERES, API_URL_CLOSE_RELEASES, API_URL_RELEASES, API_URL_TOP];
     const category = document.querySelectorAll('.category')
 
@@ -43,21 +45,150 @@ document.addEventListener("DOMContentLoaded", () => {
             category[i].classList.add('chose');
         })
     }
-
-    // const KEYWORD = document.querySelector(‘.header__search’).value.trim();
-    //найти фильм по ключевому слову
+    //seach by the keyword
     document.getElementById('searchInput').addEventListener('keypress', (e)=>{
         if (e.key === "Enter") {
-            const KEYWORD = document.querySelector('searchInput').value;
+            const KEYWORD = document.getElementById('searchInput').value.trim();
             const API_URL_KEYWORD = `https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${KEYWORD}&page=1`;
             getMovies(API_URL_KEYWORD);
             searchInput.value = "";
             console.log(KEYWORD);
         }
     })
-    //стартовая страница
-    getMovies(API_URL_PREMIERES);
+    //main page
+    getMovies(API_URL_TOP);
     category[0].classList.add('chose')
+
+    //data format
+    function typicalData(data){
+        if (data.releases){
+            data.items = data.releases
+            delete data.releases
+        }else if(data.films){
+            data.items = data.films
+            delete data.films
+        }
+        let selectedMovies = data.items.map(movie => {
+            if (movie.filmId && movie.rating){
+                return {
+                    id: movie.filmId,
+                    movieName: movie.nameRu,
+                    year: movie.year,
+                    movieUrl : movie.posterUrl,
+                    genre: movie.genres.map(item => item.genre).join(', '),
+                    rating: movie.ratingKinopoisk,
+                };
+            } else if (!movie.rating && !movie.ratingKinopoisk){
+                return {
+                    id: movie.kinopoiskId,
+                    movieName: movie.nameRu,
+                    year: movie.year,
+                    movieUrl : movie.posterUrl,
+                    genre: movie.genres.map(item => item.genre).join(', '),
+                };
+            }
+            else {
+                return {
+                    id: movie.kinopoiskId,
+                    movieName: movie.nameRu,
+                    year: movie.year,
+                    movieUrl : movie.posterUrl,
+                    genre: movie.genres.map(item => item.genre).join(', '),
+                    rating: movie.ratingKinopoisk,
+                };
+            }
+        });
+        console.log(category)
+        showMovies(selectedMovies);
+        checkLiked(selectedMovies);
+        console.log(selectedMovies);
+    }
+    //displaying movies on the page
+    function showMovies(data){
+        const moviesEl = document.querySelector(".movies");
+        document.querySelector(".movies").innerHTML = "";
+        
+        data.forEach(item =>{
+            const movieEl = document.createElement("div");
+            movieEl.classList.add("item");
+            movieEl.id = item.id;
+            movieEl.innerHTML = `
+                <div class="movie-cover-inner">
+                    <img class="movie-cover" name="movieUrl" src="${item.movieUrl}" alt=“”>
+                </div>
+                <div class="movie-info">
+                    <div class=“movie-text”>
+                        <div class="movie-title" name="movieName">${item.movieName}</div>
+                        <div class="movie-category" name="genre">${item.genre}</div>
+                        <div class="movie-year" name="year">${item.year}</div>
+                    </div>
+                    <div class="movie-average movie-average-${getClassByRate(item.rating)}">${item.rating}</div>
+                    <button class="favorite"></button>
+                </div>
+            `;
+            moviesEl.appendChild(movieEl);
+        })
+        like();
+    }
+    //clicking the like button and sending data to localStorage
+    const LS = localStorage;
+    let movieData = {};
+    let movieDataArray = [];
+    function like(){
+        const hearts = document.querySelectorAll('.favorite');
+        const movieTitle = document.querySelectorAll('.movie-title');
+        const movieUrl = document.querySelectorAll('.movie-cover')
+        const genres = document.querySelectorAll('.movie-category');
+        const year = document.querySelectorAll('.movie-year');
+        const rating = document.querySelectorAll('.movie-average');
+        const movie = document.querySelectorAll('.item');
+        
+        for (let i = 0; i < hearts.length; i++){
+            //when clicking the heart
+            hearts[i].addEventListener('click', ()=>{
+                if (hearts[i].classList.contains('liked')){
+                    hearts[i].classList.remove('liked');
+                    movieDataArray = movieDataArray.filter(movie => movie.movieName !== movieTitle[i].textContent);
+                    console.log(movieDataArray)
+                    LS.setItem('movieData', JSON.stringify(movieDataArray))
+                }else{
+                    movieData = {};
+                    hearts[i].classList.add('liked');
+                    movieData['id'] = movie[i].id;
+                    movieData[movieTitle[i].getAttribute('name')] = movieTitle[i].textContent;
+                    movieData[movieUrl[i].getAttribute('name')] = movieUrl[i].src;
+                    movieData[genres[i].getAttribute('name')] = genres[i].textContent;
+                    movieData[year[i].getAttribute('name')] = year[i].textContent;
+                    movieData[rating[i].getAttribute('name')] = rating[i].textContent;
+                    movieData['status'] = 'liked';
+                    movieDataArray.push(movieData);
+                    LS.setItem('movieData', JSON.stringify(movieDataArray))
+                }
+            });
+        }
+    }
+    //checking after updates
+    function checkLiked(data){
+        if (LS.getItem('movieData')){
+            const movieDataArray = JSON.parse(LS.getItem('movieData'));
+            const hearts = document.querySelectorAll('.favorite');
+            for (let i = 0; i < movieDataArray.length; i++){
+                for (let j = 0; j < data.length; j++){
+                    if (+movieDataArray[i].id === +data[j].id){
+                        hearts[j].classList.add('liked');
+                    }
+                }
+            }
+        }
+    }
+    //clicking to the favorite category
+    function favoriteMovies(event){
+        event.preventDefault()
+        movieDataArray = JSON.parse(LS.getItem('movieData'));
+        showMovies(movieDataArray);
+        checkLiked(movieDataArray)
+    }
+    document.querySelector('.favorites').addEventListener('click', favoriteMovies)
 })
 
 function getClassByRate(vote) {
@@ -66,45 +197,11 @@ function getClassByRate(vote) {
     } 
     else if (vote > 5) {
         return "orange";
-    } 
-    else {
+    }
+    else if (vote < 5){
         return "red";
     }
+    else {
+        return "nothing";
+    }
 }
-
-function showMovies(data) {
-    const moviesEl = document.querySelector(".movies");
-
-    document.querySelector(".movies").innerHTML = "";
-
-    data.items.map((movie) => {
-        const movieEl = document.createElement("div");
-        movieEl.classList.add("movie");
-        movieEl.innerHTML = `
-            <div class="movie-cover-inner">
-            <img
-                src="${movie.posterUrlPreview}"
-                class="movie-cover"
-                alt="${movie.nameRu}"
-            />
-            <div class="movie-cover--darkened"></div>
-        </div>
-        <div class="movie-info">
-            <div class="movie-title">${movie.nameRu}</div>
-            <div class="movie-category">${movie.genres.map(
-                (genre) => ` ${genre.genre}`
-              )}</div>
-              ${
-                movie.ratingKinopoisk &&
-                `
-                <div class="movie-average movie-average-${getClassByRate(movie.ratingKinopoisk)}">${movie.ratingKinopoisk}</div>
-              `
-              }
-            
-            <div class="movie-year">${movie.year}</div>
-        </div>
-    `;
-    moviesEl.appendChild(movieEl);
-    });
-}
-
